8307f80fea2cb2968d9655e98d3d0441cc8b1832
Steven Mirabito <steven@stevenmirabito.com>
2016-05-02 00:39:32 -0400

    Update release script

--- release.sh
+++ release.sh
@@ -3,13 +3,15 @@ if [ $# -eq 0 ]
   then
     echo "Usage: release.sh <version>"
 fi
-mkdir -p $1/bootstrapdefault
-mkdir -p $1/bootstrapdefault/bootstrap/dist
-mkdir -p $1/bootstrapdefault/selectize.js/dist
-cp -rv bootstrap/dist/* $1/bootstrapdefault/bootstrap/dist
-cp -rv selectize.js/dist/* $1/bootstrapdefault/selectize.js/dist
-cp -rv admin fixplugins.css img include index.php js language less local_head.tpl screenshot.png template theme.css themeconf.inc.php $1/bootstrapdefault/
+mkdir -p $1/csh
+mkdir -p $1/csh/bootstrap/dist
+mkdir -p $1/csh/selectize.js/dist
+mkdir -p $1/csh/bootstrap-csh/release
+cp -rv bootstrap/dist/* $1/csh/bootstrap/dist
+cp -rv selectize.js/dist/* $1/csh/selectize.js/dist
+cp -rv bootstrap-csh/dist/* $1/csh/bootstrap-csh/release
+cp -rv admin fixplugins.css img include index.php js language less local_head.tpl screenshot.png template theme.css themeconf.inc.php $1/csh/
 cd $1
-zip -r ../bootstrapdefault-$1.zip bootstrapdefault
+zip -r ../csh-$1.zip csh
 cd ..
 rm -r $1
