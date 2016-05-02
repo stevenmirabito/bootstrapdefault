cf436c8e60511645463d29498b390b8d97972365
Steven Mirabito <steven@stevenmirabito.com>
2016-05-02 15:59:36 -0400

    Move sidebar up

--- js/theme.js
+++ js/theme.js
@@ -56,7 +56,6 @@ $(document).ready(function() {
     var sidebar = $("#sidebar");
     var theImage = $('#theImage')
     if (sidebar.length && theImage.length) {
-        sidebar.css('top', (theImage.offset().top + 20) + 'px');
         $('#info-link').click(function () {
             var sidebar = $('#sidebar');
             if (parseInt(sidebar.css('right')) < 0) {
